52cc7605a5237f3b9097a296468318f886420d05
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2024-08-27 01:13:15 +0600

    redux added to login user

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import Store from "./redux/store";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   // <React.StrictMode>
+//   <App />
+//   // </React.StrictMode>
+// );
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
